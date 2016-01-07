7952c04b9015b06b104079cbbab65f14e4e61647
Steve Massicotte <steve.massicotte@umontreal.ca>
2016-01-06 21:32:48 -0500

    Changement de version (0.5).

--- www/config.prod.php
+++ www/config.prod.php
@@ -5,4 +5,4 @@ define('RACINE', '/app');
 require_once(RACINE . '/routine/classes/configApp.php');
 
 global $configApp;
-$configApp = new configApp(NIVEAU, "0.4 (GITLOG)", RACINE);
+$configApp = new configApp(NIVEAU, "0.5 (GITLOG)", RACINE);
